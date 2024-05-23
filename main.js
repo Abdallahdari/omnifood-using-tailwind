08211ba13f90fc1296dd0a97d49f7300ce2bf515
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-05-23 20:54:53 +0300

    adding price part

---
+++ main.js
@@ -0,0 +1,28 @@
+const info={
+email:'hooyo@gmail.com',
+pin:'1234',
+
+}
+
+
+const main=document.querySelector('.hidden')
+const login=document.querySelector('.login-part')
+const btn=document.querySelector('.btn')
+const inputemail=document.getElementById('login-email')
+const inputpass=document.getElementById('login-password')
+
+// event handler 
+btn.addEventListener('click',function(e){
+e.preventDefault()
+    if(inputemail.value===info.email && inputpass.value===info.pin){
+        
+        main.classList.remove('hidden')
+        login.style.display='none'
+    }
+        else 
+alert('worng passwrod or email')
+
+
+
+
+})
