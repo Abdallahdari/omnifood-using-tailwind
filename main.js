504d33d3a97257300f014ebd4fe260037d7bbd42
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-05-24 02:55:26 +0300

    adding tailwind full

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 const info={
-email:'hooyo@gmail.com',
-pin:'1234',
-
-}
-
-
-const main=document.querySelector('.hidden')
-const login=document.querySelector('.login-part')
-const btn=document.querySelector('.btn')
-const inputemail=document.getElementById('login-email')
-const inputpass=document.getElementById('login-password')
-
-// event handler 
-btn.addEventListener('click',function(e){
-e.preventDefault()
-    if(inputemail.value===info.email && inputpass.value===info.pin){
-        
-        main.classList.remove('hidden')
-        login.style.display='none'
+    email:'hooyo@gmail.com',
+    pin:'1234',
+    
     }
-        else 
-alert('worng passwrod or email')
-
-
-
-
-})
+    const info2={
+      email:'aabe@gmail.com',
+      pin:'1234',
+    
+    }
+    const accounts=[info,info2]
+    
+    const main=document.querySelector('.hidden')
+    const login=document.querySelector('.login-part')
+    const btn=document.querySelector('.btn')
+    // tijabo
+    const inputemail=document.getElementById('login-email')
+    const inputpass=document.getElementById('login-password')
+    
+    // event handler 
+    btn.addEventListener('click',function(e){
+    e.preventDefault()
+          
+      const account = accounts.find(acc => acc.email === inputemail.value && acc.pin === inputpass.value)
+    
+      if (account) {
+        alert('hello')
+        // Perform login action, e.g., show main content, hide login part, etc.
+        main.classList.remove('hidden')
+        login.classList.add('hidden')
+      } else {
+        alert('wrong user or pass')
+      }
+            
+    
+    
+    
+    
+    })
