08b3041d715d542d71ae29b51ac9734a1c388279
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-05-31 23:09:04 +0300

    added the forms

--- main.js
+++ main.js
@@ -1,62 +1,41 @@
-const info={
-<<<<<<< HEAD
-    email:'hooyo@gmail.com',
-    pin:'1234',
-    
-=======
-email:'hooyo@gmail.com',
-pin:'1234',
+const info = {
+  email: 'hooyo@gmail.com',
+  pin: '1234',
+};
 
-}
+const info2 = {
+  email: 'aabe@gmail.com',
+  pin: '1234',
+};
 
+const accounts = [info, info2];
 
-const main=document.querySelector('.hidden')
-const login=document.querySelector('.login-part')
-const btn=document.querySelector('.btn')
-// tijabo
-const inputemail=document.getElementById('login-email')
-const inputpass=document.getElementById('login-password')
+const main = document.querySelector('.hidden');
+const login = document.querySelector('.login-part');
+const btn = document.querySelector('.btn');
+const inputemail = document.getElementById('login-email');
+const inputpass = document.getElementById('login-password');
 
-// event handler 
-btn.addEventListener('click',function(e){
-e.preventDefault()
-    if(inputemail.value===info.email && inputpass.value===info.pin){
-        
-        main.classList.remove('hidden')
-        login.style.display='none'
->>>>>>> origin/main
-    }
-    const info2={
-      email:'aabe@gmail.com',
-      pin:'1234',
-    
-    }
-    const accounts=[info,info2]
-    
-    const main=document.querySelector('.hidden')
-    const login=document.querySelector('.login-part')
-    const btn=document.querySelector('.btn')
-    // tijabo
-    const inputemail=document.getElementById('login-email')
-    const inputpass=document.getElementById('login-password')
-    
-    // event handler 
-    btn.addEventListener('click',function(e){
-    e.preventDefault()
-          
-      const account = accounts.find(acc => acc.email === inputemail.value && acc.pin === inputpass.value)
-    
-      if (account) {
-        alert('hello')
-        // Perform login action, e.g., show main content, hide login part, etc.
-        main.classList.remove('hidden')
-        login.classList.add('hidden')
-      } else {
-        alert('wrong user or pass')
-      }
-            
-    
-    
-    
-    
-    })
+// Event handler 
+// btn.addEventListener('click', function (e) {
+//   e.preventDefault();
+  
+//   const account = accounts.find(acc => acc.email === inputemail.value && acc.pin === inputpass.value);
+  
+//   if (account) {
+//       alert('Hello');
+//       // Perform login action, e.g., show main content, hide login part, etc.
+//       main.classList.remove('hidden');
+//       login.style.display = 'none';
+//   } else {
+//       alert('Wrong user or password');
+//   }
+// });
+const clickbtn = document.getElementById('icon-btn');
+const sm_screen = document.querySelector('.sm-screem');
+
+clickbtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  sm_screen.classList.toggle('h-hidden');
+
+});
